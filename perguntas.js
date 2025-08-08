13b2bd9302ec698a31b3b63ec7f507cf1f79c77c
Falls740 <thomas.verner@escola.pr.gov.br>
2025-08-08 12:07:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Jogos', 
+    'Qual jogo Mais Vendido na História?', 
+    'Minecraft'  
+ )
+ 
+ 
+ criaCartao(
+    'Música',  
+    'Qual a música mais famosa da banda LinkinPark', 
+    'Numb', 
+ )
+ 
+ 
+ criaCartao(
+    'Filmes', 
+    'A continuação de um filme de golfe do Adam Sandler ', 
+    'Um Maluco No Golfe 2' 
+ )
+ 
+ 
+ criaCartao(
+    'Futebol', 
+    'Maior campẽo da Champions League', 
+    'Real Madrid' 
+ 
+ )
+ 
